2b711cd112f79264f93dbdd19012390c6097be5e
Carl Philipp Reh <carlphilippreh@gmail.com>
2015-05-31 00:03:11 +0200

    Update headers

--- include/majutsu/memory/detail/init_raw_memory.hpp
+++ include/majutsu/memory/detail/init_raw_memory.hpp
@@ -10,8 +10,8 @@
 #include <majutsu/role_return_type.hpp>
 #include <majutsu/role_tag_fwd.hpp>
 #include <fcppt/config/external_begin.hpp>
-#include <utility>
 #include <type_traits>
+#include <utility>
 #include <fcppt/config/external_end.hpp>
 
 
